5f1ffe7d6c1b363e7e39e9d94cca5aae921b19cf
2837329a <2837329a@student.gla.ac.uk>
2023-09-26 09:28:07 +0800

    Added mock restaurant profile page

--- reservation-webapp/main.py
+++ reservation-webapp/main.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
 
+@app.route("/restaurant/abc") #replace abc dynamically, restaurant remains static
+def restaurant_profile():
+    return render_template('restaurant_profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
