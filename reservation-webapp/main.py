16870b5432feea27eff7312f7f870c6004426e26
“jeffrey1002” <zaichrist@hotmail.com>
2023-09-30 20:41:53 +0800

    change pw

--- reservation-webapp/main.py
+++ reservation-webapp/main.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'secretsecret'
 app.config['MYSQL_DB'] = 'clouddb2'
 app.config['MYSQL_USER'] = 'admin'
 app.config['MYSQL_HOST'] = 'aws-cloud-db.c51u88x7edpw.us-east-1.rds.amazonaws.com'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = 'pmB6YW7fYDuRo0Be8sEJ'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
