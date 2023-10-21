8192ecb6499175b90d1b2895bc200b035eb1af9f
nkjcolin <colinngkj254@gmail.com>
2023-10-21 22:11:43 +0800

    update

--- webscrape/scrape.py
+++ webscrape/scrape.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
-from selenium.common.exceptions import StaleElementReferenceException
 import json
 
 # Global Config
