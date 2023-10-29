0ea58869a352828df6126bcf0d5cbf83dfe5d7dd
2837329a <2837329a@student.gla.ac.uk>
2023-10-30 01:24:59 +0800

    update json parsing function

--- webscrape/json_parse.py
+++ webscrape/json_parse.py
@@ -13,11 +13,19 @@ def bring_data_over_mysql():
 
     with open('restaurant_data_final.json', 'r') as json_file:
         data = json.load(json_file)
+        all_reviews =  '['
 
         for item in data:
-            query = "INSERT INTO restaurants (name, location, image, rating, numberofrating, description) VALUES (%s, %s, %s, %s, %s, %s)"
-            cursor.execute(query, (item['name'], item['location'], item['imageLink'], item['rating'], item['numOfRating'], item['description']))
-        
+            query = "INSERT INTO restaurants (name, location, image, rating, numberofrating, description, timings) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(query, (item['name'], item['location'], item['imageLink'], item['rating'], item['numOfRating'], item['description'], item['timeslot']))
+
+            for review in item['review']:
+                all_reviews =  all_reviews + "'" + review + "', "
+
+            all_reviews = all_reviews[:-2] + ']'
+            query = "INSERT INTO restaurants (reviews) VALUES (%s)"
+            cursor.execute(query, all_reviews)
+
     connection.commit()
     connection.close()
 
