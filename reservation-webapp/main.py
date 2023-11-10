ac059dc7906862d712675883acebba1263593db0
nkjcolin <colinngkj254@gmail.com>
2023-11-10 13:34:27 +0800

    update search query

--- reservation-webapp/main.py
+++ reservation-webapp/main.py
@@ -42,18 +42,21 @@ def search_restaurants():
 
     # Add search_query condition if it is provided
     if search_query:
-        query += " AND name LIKE %s AND rating >= 3.5 ORDER BY rating DESC"
+        query += " AND name LIKE %s AND rating >= 3.5"
         params.append(f"%{search_query}%")
 
     # Add dietary_needs condition if it is provided
     if dietary_needs:
-        query += " AND dietary_needs = %s AND rating >= 3.5 ORDER BY rating DESC"
+        query += " AND dietary_needs = %s AND rating >= 3.5"
         params.append(dietary_needs)
 
     # Add meal_type condition if it is provided
     if meal_type:
-        query += " AND meal_type = %s AND rating >= 3.5 ORDER BY rating DESC"
+        query += " AND meal_type = %s AND rating >= 3.5"
         params.append(meal_type)
+
+    if(search_query or dietary_needs or meal_type):
+        query += " ORDER BY rating DESC"
 
     # Execute the SQL query with the appropriate parameters
     cursor.execute(query, params)
